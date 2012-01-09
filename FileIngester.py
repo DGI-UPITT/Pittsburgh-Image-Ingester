5cf3a8e4962cdbef0ad0bf25e41b4c7723efc892
Jason MacWilliams <jason@discoverygarden.ca>
2012-01-09 14:11:11 -0400

    exported a jpg instead of jp2 for iiv compatibility removed "garbage" from pid since testing is beyond that point added a hack to force the mods datastream inline - need to parameterize this somehow

--- FileIngester.py
+++ FileIngester.py
@@ -1,14 +1,11 @@
 from islandoraUtils import fileConverter as converter
-from commonFedora import *
+from utils.commonFedora import *
 
 """ ====== INGEST A SINGLE OBJECT ====== """
 def createObjectFromFiles(fedora, config, objectData):
     """
-    Create a bunch of fedora objects (1 for each tif in @folder)
+    Create a fedora object containing all the data in objectData and more
     """
-
-    if config.dryrun:
-        return True
 
     for ds in [ "TIFF", "MODS" ]:
         # some error checking
@@ -17,12 +14,14 @@ def createObjectFromFiles(fedora, config, objectData):
             print("Object data is missing required datastream: %s" % ds)
             return False
 
-
     #objPid = fedora.getNextPID(config.fedoraNS)
-    objPid = "hpitt:garbage-%s" % objectData['label']
+    objPid = "hpitt:%s" % objectData['label']
 
     #extraNamespaces = { 'pageNS' : 'info:islandora/islandora-system:def/pageinfo#' }
     #extraRelationships = { fedora_relationships.rels_predicate('pageNS', 'isPageNumber') : str(idx+1) }
+
+    if config.dryrun:
+        return True
 
     # create the object (page)
     try:
@@ -33,16 +32,30 @@ def createObjectFromFiles(fedora, config, objectData):
 
     # ingest the datastreams we were given
     for dsid, file in objectData['datastreams'].iteritems():
-        fedoraLib.update_datastream(obj, dsid, file, label=unicode(os.path.basename(file)), mimeType=misc.getMimeType(os.path.splitext(file)[1]))
+        # hard coded blarg:
+        if dsid in ["MODS"]:
+            controlGroup = "X"
+        else:
+            controlGroup = "M"
+        fedoraLib.update_datastream(obj, dsid, file, label=unicode(os.path.basename(file)), mimeType=misc.getMimeType(os.path.splitext(file)[1]), controlGroup=controlGroup)
     #fedoraLib.update_datastream(obj, "TIFF", tifFile, label=unicode("%s.tif" % basePage), mimeType=misc.getMimeType("tiff"))
 
     # ingest my custom datastreams for this object
-    # create a JP2 datastream
+    # create a JPG datastream
     tifFile = objectData['datastreams']['TIFF']
     baseName = os.path.splitext(os.path.basename(tifFile))[0]
-    jp2File = os.path.join(config.tempDir, "%s.jp2" % baseName)
-    converter.tif_to_jp2(tifFile, jp2File, 'default', 'default') # this will generate jp2File
-    fedoraLib.update_datastream(obj, "JP2", jp2File, label=unicode("%s.jp2" % baseName), mimeType=misc.getMimeType("jp2"))
-    os.remove(jp2File) # finished with that
+    jpgFile = os.path.join(config.tempDir, "%s.jpg" % baseName)
+    converter.tif_to_jpg(tifFile, jpgFile, ['-compress', 'JPEG', '-quality', '90%']) # this will generate jpgFile
+    fedoraLib.update_datastream(obj, u"JPG", jpgFile, label=unicode("%s.jpg" % baseName), mimeType=misc.getMimeType("jp2"))
+    os.remove(jpgFile) # finished with that
+
+    # ingest my custom datastreams for this object
+    # create a JP2 datastream
+    #tifFile = objectData['datastreams']['TIFF']
+    #baseName = os.path.splitext(os.path.basename(tifFile))[0]
+    #jp2File = os.path.join(config.tempDir, "%s.jp2" % baseName)
+    #converter.tif_to_jp2(tifFile, jp2File, 'default', 'default') # this will generate jp2File
+    #fedoraLib.update_datastream(obj, s"JP2", jp2File, label=unicode("%s.jp2" % baseName), mimeType=misc.getMimeType("jp2"))
+    #os.remove(jp2File) # finished with that
 
     return True
