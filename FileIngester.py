69da1b1704335776d574f1364b6e1af82c4087c8
Jason MacWilliams <jason@discoverygarden.ca>
2012-01-13 16:49:40 -0400

    changed jpg datastream to jp2 datastream, starting to implement jhove mix extraction

--- FileIngester.py
+++ FileIngester.py
@@ -41,21 +41,30 @@ def createObjectFromFiles(fedora, config, objectData):
     #fedoraLib.update_datastream(obj, "TIFF", tifFile, label=unicode("%s.tif" % basePage), mimeType=misc.getMimeType("tiff"))
 
     # ingest my custom datastreams for this object
+
     # create a JPG datastream
-    tifFile = objectData['datastreams']['TIFF']
-    baseName = os.path.splitext(os.path.basename(tifFile))[0]
-    jpgFile = os.path.join(config.tempDir, "%s.jpg" % baseName)
-    converter.tif_to_jpg(tifFile, jpgFile, ['-compress', 'JPEG', '-quality', '90%']) # this will generate jpgFile
-    fedoraLib.update_datastream(obj, u"JPG", jpgFile, label=unicode("%s.jpg" % baseName), mimeType=misc.getMimeType("jp2"))
-    os.remove(jpgFile) # finished with that
+    #tifFile = objectData['datastreams']['TIFF']
+    #baseName = os.path.splitext(os.path.basename(tifFile))[0]
+    #jpgFile = os.path.join(config.tempDir, "%s.jpg" % baseName)
+    #converter.tif_to_jpg(tifFile, jpgFile, ['-compress', 'JPEG', '-quality', '90%']) # this will generate jpgFile
+    #fedoraLib.update_datastream(obj, u"JPG", jpgFile, label=unicode("%s.jpg" % baseName), mimeType=misc.getMimeType("jpg"))
+    #os.remove(jpgFile) # finished with that
 
     # ingest my custom datastreams for this object
     # create a JP2 datastream
-    #tifFile = objectData['datastreams']['TIFF']
-    #baseName = os.path.splitext(os.path.basename(tifFile))[0]
-    #jp2File = os.path.join(config.tempDir, "%s.jp2" % baseName)
-    #converter.tif_to_jp2(tifFile, jp2File, 'default', 'default') # this will generate jp2File
-    #fedoraLib.update_datastream(obj, s"JP2", jp2File, label=unicode("%s.jp2" % baseName), mimeType=misc.getMimeType("jp2"))
-    #os.remove(jp2File) # finished with that
+    tifFile = objectData['datastreams']['TIFF']
+    baseName = os.path.splitext(os.path.basename(tifFile))[0]
+    jp2File = os.path.join(config.tempDir, "%s.jp2" % baseName)
+    converter.tif_to_jp2(tifFile, jp2File, 'default', 'default') # this will generate jp2File
+    fedoraLib.update_datastream(obj, s"JP2", jp2File, label=unicode("%s.jp2" % baseName), mimeType=misc.getMimeType("jp2"))
+    os.remove(jp2File) # finished with that
+
+    # extract mix metadata
+    #cmd= jhove -h xml $INFILE | xsltproc jhove2mix.xslt - > `basename ${$INFILE%.*}.mix`
+    #jhoveCmd = ["jhove", "-h", "xml", tifFile]
+    #r = popen(jhoveCmd, shell=True, stdout=tmp)
+    #if r != 0:
+    #    # failed for some reason
+    #    print("jhove conversion failed")
 
     return True
